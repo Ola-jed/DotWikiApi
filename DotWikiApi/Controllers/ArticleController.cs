9bd50156fc2fa224ac2b6a373dd6f88b4592e052
olabijed@gmail.com <olabijed@gmail.com>
2021-08-28 10:51:01 +0100

    Article controller implementation started but not yet finished

---
+++ DotWikiApi/Controllers/ArticleController.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DotWikiApi.Authentication;
+using DotWikiApi.Data;
+using DotWikiApi.Dtos;
+using DotWikiApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotWikiApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly IArticleRepository _articleRepository;
+        private readonly IMapper _mapper;
+        private readonly UserManager<ApplicationUser> _userManager;
+        
+        public ArticleController(IArticleRepository articleRepository,IMapper mapper, UserManager<ApplicationUser> userManager)
+        {
+            _articleRepository = articleRepository;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+        
+        // GET: api/Article
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable>> GetAll()
+        {
+            var articles = _mapper.Map<IEnumerable<ArticleReadDto>>(await _articleRepository.GetAllArticles());
+            return Ok(articles);
+        }
+
+        // GET: api/Article/5
+        [HttpGet("{id:int}", Name = "Get")]
+        public async Task<ActionResult<Article>> Get(int id)
+        {
+            var article = await _articleRepository.GetArticle(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+            return Ok(article);
+        }
+
+        // POST: api/Article
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<ArticleReadDto>> Post(ArticleCreateDto articleCreateDto)
+        {
+            var article = _mapper.Map<Article>(articleCreateDto);
+            var usr = await _userManager.FindByNameAsync(HttpContext.User.Identity?.Name);
+            article.ApplicationUser = usr;
+            article.CreatedAt = DateTime.Now;
+            await _articleRepository.CreateArticle(article);
+            await _articleRepository.SaveChanges();
+            return CreatedAtAction(nameof(Get),new {Id = article.Id},_mapper.Map<ArticleReadDto>(article));
+        }
+
+        // PUT: api/Article/5
+        [Authorize]
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, ArticleUpdateDto articleUpdateDto)
+        {
+            var article = await _articleRepository.GetArticle(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+            // Create the snapshot corresponding to the current state
+
+            return NoContent();
+        }
+
+        // DELETE: api/Article/5
+        [Authorize]
+        [HttpDelete("{id:int}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
