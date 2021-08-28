5c6e71cf669617e37b4c48e5efc7e6f18e64aad3
olabijed@gmail.com <olabijed@gmail.com>
2021-08-28 10:50:32 +0100

    Extra migrations

---
+++ DotWikiApi/Migrations/20210827203547_UserIdFieldUpdated.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotWikiApi.Migrations
+{
+    public partial class UserIdFieldUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_AspNetUsers_ApplicationUserId1",
+                table: "Articles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_ApplicationUserId1",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId1",
+                table: "Articles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationUserId",
+                table: "Articles",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_ApplicationUserId",
+                table: "Articles",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_AspNetUsers_ApplicationUserId",
+                table: "Articles",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_AspNetUsers_ApplicationUserId",
+                table: "Articles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_ApplicationUserId",
+                table: "Articles");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ApplicationUserId",
+                table: "Articles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId1",
+                table: "Articles",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_ApplicationUserId1",
+                table: "Articles",
+                column: "ApplicationUserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_AspNetUsers_ApplicationUserId1",
+                table: "Articles",
+                column: "ApplicationUserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
