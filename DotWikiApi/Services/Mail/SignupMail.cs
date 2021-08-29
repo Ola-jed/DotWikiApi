9eb692a4fa54738f1a76c298db8310e31b743a9b
olabijed@gmail.com <olabijed@gmail.com>
2021-08-29 21:31:42 +0100

    Signup mail

---
+++ DotWikiApi/Services/Mail/SignupMail.cs
@@ -0,0 +1,35 @@
+using DotWikiApi.Authentication;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace DotWikiApi.Services.Mail
+{
+    public class SignupMail : IMailable
+    {
+        private readonly MailSettings _options;
+        private readonly ApplicationUser _user;
+
+        public SignupMail(IOptions<MailSettings> mailSettings, ApplicationUser user)
+        {
+            _options = mailSettings.Value;
+            _user = user;
+        }
+
+        public MimeMessage Build()
+        {
+            var email = new MimeMessage
+            {
+                Sender = MailboxAddress.Parse(_options.MailUser),
+                Subject = "Registration to DotWiki",
+                To = { MailboxAddress.Parse(_user.Email) }
+            };
+            var builder = new BodyBuilder
+            {
+                HtmlBody = $"Hello {_user.UserName}. We are happy to welcome you among us at DotWiki.",
+                TextBody = $"Hello {_user.UserName}. We are happy to welcome you among us at DotWiki."
+            };
+            email.Body = builder.ToMessageBody();
+            return email;
+        }
+    }
+}
