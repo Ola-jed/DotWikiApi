3ea99fa48e5ce02fcc6f30274bf68892d8e690b5
olabijed@gmail.com <olabijed@gmail.com>
2021-08-29 21:33:16 +0100

    Whitespace trimmed and snapshot repo registered in di container

--- DotWikiApi/Startup.cs
+++ DotWikiApi/Startup.cs
@@ -31,11 +31,12 @@ namespace DotWikiApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IArticleRepository, ArticleRepository>();
+            services.AddScoped<ISnapshotRepository, SnapshotRepository>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             // For Identity
             services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<DotWikiContext>()  
+                .AddEntityFrameworkStores<DotWikiContext>()
                 .AddDefaultTokenProviders();
             var builder = new NpgsqlConnectionStringBuilder
             {
@@ -48,30 +49,31 @@ namespace DotWikiApi
             cfg["MailUser"] = Configuration["MailUser"];
             cfg["MailPassword"] = Configuration["MailPassword"];
             services.Configure<MailSettings>(cfg);
+            services.AddTransient<IMailService,MailService>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DotWikiApi", Version = "v1" });
             });
-            services.AddAuthentication(options =>  
-                {  
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;  
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;  
-                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;  
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
                 })
-                .AddJwtBearer(options =>  
-                {  
-                    options.SaveToken = true;  
-                    options.RequireHttpsMetadata = false;  
-                    options.TokenValidationParameters = new TokenValidationParameters()  
-                    {  
-                        ValidateIssuer = true,  
-                        ValidateAudience = true,  
-                        ValidAudience = Configuration["JWT:ValidAudience"],  
-                        ValidIssuer = Configuration["JWT:ValidIssuer"],  
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))  
-                    };  
-                });  
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidAudience = Configuration["JWT:ValidAudience"],
+                        ValidIssuer = Configuration["JWT:ValidIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
