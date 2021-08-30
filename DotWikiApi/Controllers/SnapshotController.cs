6b18bd746516489cd51bfb83d094249d36d93ebe
olabijed@gmail.com <olabijed@gmail.com>
2021-08-30 11:01:13 +0100

    Roll back to previous snapshot method

--- DotWikiApi/Controllers/SnapshotController.cs
+++ DotWikiApi/Controllers/SnapshotController.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using DotWikiApi.Authentication;
 using DotWikiApi.Data;
 using DotWikiApi.Dtos;
+using DotWikiApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -37,7 +40,6 @@ namespace DotWikiApi.Controllers
             {
                 return NotFound();
             }
-
             return Ok(_mapper.Map<IEnumerable<SnapshotReadDto>>(await _snapshotRepository.GetSnapshots(id)));
         }
 
@@ -48,15 +50,16 @@ namespace DotWikiApi.Controllers
             return snapshot == null ? NotFound() : Ok(snapshot);
         }
 
+        [Authorize]
         [HttpDelete("Snapshot/{id:int}")]
         public async Task<ActionResult> Delete(int id)
         {
-            var currentUser = await _userManager.FindByNameAsync(HttpContext.User.Identity?.Name);
             var snapshot = await _snapshotRepository.GetSnapshot(id);
             if (snapshot == null)
             {
                 return NotFound();
             }
+            var currentUser = await _userManager.FindByNameAsync(HttpContext.User.Identity?.Name);
             if (snapshot.ApplicationUserId != currentUser.Id)
             {
                 return Forbid();
@@ -64,6 +67,38 @@ namespace DotWikiApi.Controllers
             _snapshotRepository.DeleteSnapshot(snapshot);
             return NoContent();
         }
-        // TODO : make Rollback to a previous snapshot
+
+        [Authorize]
+        [HttpPost("Snapshot/{id:int}/Rollback")]
+        public async Task<ActionResult> RollbackTo(int id)
+        {
+            var snapshot = await _snapshotRepository.GetSnapshot(id);
+            if (snapshot == null)
+            {
+                return NotFound();
+            }
+            var currentUser = await _userManager.FindByNameAsync(HttpContext.User.Identity?.Name);
+            if (snapshot.ApplicationUserId != currentUser.Id)
+            {
+                return Forbid();
+            }
+            var article = await _articleRepository.GetArticle(snapshot.ArticleId);
+            // Create a snapshot for this state
+            var newSnapshot = new Snapshot
+            {
+                Title = article.Title,
+                Content = article.Content,
+                ArticleId = article.Id,
+                ApplicationUserId = currentUser.Id,
+                CreatedAt = DateTime.Now
+            };
+            await _snapshotRepository.CreateSnapshot(newSnapshot);
+            await _snapshotRepository.SaveChanges();
+            // Update the article with the selected snapshot data
+            article.Content = snapshot.Content;
+            _articleRepository.UpdateArticle(article);
+            await _articleRepository.SaveChanges();
+            return NoContent();
+        }
     }
 }
