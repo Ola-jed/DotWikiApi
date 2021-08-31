e4b467fa4db6ddefa7596066331bc398e8ca84cd
olabijed@gmail.com <olabijed@gmail.com>
2021-08-31 17:01:09 +0100

    Created a service for user account management

---
+++ DotWikiApi/Services/User/IApplicationUserService.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DotWikiApi.Dtos;
+using DotWikiApi.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace DotWikiApi.Services.User
+{
+    public interface IApplicationUserService
+    {
+        Task<ApplicationUser> FindUserById(string id);
+        Task<ApplicationUser> FindUserByUserName(string userName);
+        Task<IList<string>> GetUserRoles(ApplicationUser applicationUser);
+        Task<IdentityResult> CreateUser(ApplicationUser user, string password);
+        Task<IdentityResult> UpdateUser(ApplicationUser initialValue, AccountUpdateDto updateDto);
+        Task<IdentityResult> DeleteUser(ApplicationUser user);
+        Task<bool> CheckPassword(string username, string password);
+    }
+}
