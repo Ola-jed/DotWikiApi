aecb622a2c53afea0d86dbbb3690b98be0bee5ac
olabijed@gmail.com <olabijed@gmail.com>
2021-08-31 13:09:41 +0100

    Constants --> Read only properties

--- DotWikiApi/Models/UserRoles.cs
+++ DotWikiApi/Models/UserRoles.cs
@@ -2,7 +2,7 @@ namespace DotWikiApi.Models
 {
     public static class UserRoles
     {
-        public const string Admin = "Admin";
-        public const string User = "User";
+        public static string Admin { get; } = "Admin";
+        public static string User { get; } = "User";
     }
 }
